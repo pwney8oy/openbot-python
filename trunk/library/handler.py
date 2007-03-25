44081df91b257df3a7211a2894963314c846b2db
luciano.ferraro <luciano.ferraro@5573e327-3421-0410-ada2-51a86d8d448c>
2007-03-25 10:42:26 +0000

    Copryright fixes - added handler.py

--- trunk/library/handler.py
+++ trunk/library/handler.py
@@ -22,4 +22,51 @@
 #    Free Software Foundation, Inc.,                                       #
 #    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
 ############################################################################
-__all__ = ['feedparser', 'utils', 'handler']
+"""
+ *** Still not used in OpenBot ***
+"""
+from utils import messages as send
+send = send()
+base = {}
+
+class add:
+    def __init__(self):
+        send.system("PHandler's add class is loading...")
+    def init(self, name, receiver):
+        if base.has_key(name):
+            warning("%s was handled before, replacing it" % name)
+        base[name] = receiver
+
+class remove:
+    def __init__(self):
+        send.system("PHandler's remove class is loading...")
+    def init(self, name):
+        if base.has_key(name):
+            base.remove(name)
+            return 1
+        else:
+            warning("%s isn't in the handler's list" % name)
+            return 0
+
+class call:
+    def __init__(self):
+        send.system("PHandler's call class is loading...")
+    def init(self, name, *datas):
+        if base.has_key(name):
+            base[name](*datas)
+            return 1
+        else:
+            warning("%s isn't in the handler's list" % name)
+            return 0
+
+class Core:
+    add = add().init
+    remove = remove().init
+    call = call().init
+    def __init__(self):
+        send.system("PHandler succerfull loaded")
+
+#if __name__ == "__main__":
+def test(message):
+    print message
+core = Core()
